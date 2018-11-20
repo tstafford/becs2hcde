a91b20a4c7cb6083c53e412a5b322eb57ac7b20d
Tyler Stafford <tstafford@hcbb.com>
2018-11-20 09:54:40 -0800

    Removed absolute path.

--- batch.sh
+++ batch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # this will submit the job to batch so we don't overload the server when we trigger all these jobs to run at midnight
 cat << EOM | batch > /dev/null 2>&1
-/opt/queries/HemaConnect_DonorEligibility/updateDonorEligibility.sh
+$(dirname $0)/updateDonorEligibility.sh
 EOM
